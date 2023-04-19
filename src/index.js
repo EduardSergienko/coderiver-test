6f6eab81b0e2bf95b4892b7afb2c695488ae0619
Eduard <katleraster@gmail.com>
2023-04-19 10:06:01 +0300

    fix: :bug: enable StrictMode

--- src/index.js
+++ src/index.js
@@ -8,10 +8,9 @@ import "./custom-swiper-styles.scss";
 import { BrowserRouter } from "react-router-dom";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  // <React.StrictMode>
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
-
-  // </React.StrictMode>
+  <React.StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>
 );
